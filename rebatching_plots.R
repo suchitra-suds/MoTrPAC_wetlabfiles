a1c6ba4a579aa1dfd2574345f81e113650c7cb58
Suchitra <139289273+suchitra-suds@users.noreply.github.com>
2025-01-10 15:36:35 -0800

    Update rebatching_plots.R

--- rebatching_plots.R
+++ rebatching_plots.R
@@ -4,20 +4,19 @@ library(ggplate)
 samples=read.csv('/Users/ssuds/Desktop/motrpac/tranche_3_muscle_rebatching.csv')
 data=samples
 
-# Install ggplate package if not already installed
+
 # devtools::install_github("rolkra/ggplate")
 
-# Load required libraries
+
 library(ggplate)
 library(dplyr)
 
 
-# Function to generate a ggplate for each unique Box
 generate_ggplate <- function(data, box_id) {
   # Filter data for the specific box
   box_data <- data %>% filter(Box == box_id)
   
-  # Create the ggplate
+
   p <- plate_plot(
     data = box_data,
     position = Position,
@@ -29,20 +28,16 @@ generate_ggplate <- function(data, box_id) {
     show_legend = TRUE
   ) +
     ggtitle(paste("Plate for Box:", box_id)) +
- # Use a color scale for better visualization
     theme_minimal() 
   
-  # Print the plot
   print(p)
   
-  # Save the plot as an image file
   ggsave(paste0("ggplate_", box_id, ".png"), plot = p, width = 8, height = 6)
 }
 
-# Get unique box IDs
 unique_boxes <- unique(data$Box)
 
-# Generate and save ggplate for each unique Box
+#saves plot for each unique box
 for (box_id in unique_boxes) {
   generate_ggplate(data, box_id)
 }
